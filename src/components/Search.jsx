c3cee60499a6f6e9d806fcd647854d82b82e539e
orkunoci <devorkunoci@gmail.com>
2022-08-30 18:58:45 +0300

    bug fix

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,6 @@ import {MenuItem, Paper,MenuList } from "@mui/material";
 import InputAdornment from '@mui/material/InputAdornment';
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
-import CloseIcon from "@mui/icons-material/Close";
 import axios from "axios";
 
 
